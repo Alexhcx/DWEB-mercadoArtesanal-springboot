49f55da3fd35497ab30ac588db764af272742853
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2023-12-11 20:24:32 -0300

    Backend SpringBoot

--- src/main/java/com/example/puc/mercadoartesanal/models/Produtos.java
+++ src/main/java/com/example/puc/mercadoartesanal/models/Produtos.java
@@ -3,7 +3,7 @@ package com.example.puc.mercadoartesanal.models;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import jakarta.persistence.GenerationType;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -26,12 +26,23 @@ public class Produtos implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long idProduto;
+
     private String nomeProduto;
+
     private String tipoProduto;
+
+    @Column(name = "descricao", length = 5000)
     private String descricao;
+
+    @Column(name = "imagem1", length = 1000)
     private String imagem1;
+    
+    @Column(name = "imagem2", length = 1000)
     private String imagem2;
+
+    @Column(name = "imagem3", length = 1000)
     private String imagem3;
+
     private BigDecimal preco;
 
 }
